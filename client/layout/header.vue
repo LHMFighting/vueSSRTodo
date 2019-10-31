56ad6d0ed551249517fb6d92e613116e98aaca0f
lvhm1 <lvhm1@meicloud.com>
2019-10-31 16:04:49 +0800

    header提交

--- client/layout/header.vue
+++ client/layout/header.vue
@@ -1,23 +1,23 @@
 <template>
-    <header class="main-header">
-        <h1>JTodo</h1>
-    </header>
+  <header :class="$style.mainHeader">
+    <h1>JTodo</h1>
+  </header>
 </template>
 
 <script>
 export default {
-    
+
 }
 </script>
 
-<style lang="stylus" scoped>
+<style module>
 .main-header {
-    text-align center
-    h1 {
-        font-size 100px
-        color rgba(175, 47, 47, 0.4)
-        font-weight 100
-        margin 20px
-    }
+    text-align: center;
+}
+h1 {
+    font-size: 100px;
+    color: rgba(175, 47, 47, 0.4);
+    font-weight: 100;
+    margin: 20px;
 }
 </style>
